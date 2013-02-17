3501bffdaf1d8da9e72610a8f3292276e4d54882
Rodrigo Manhães <rmanhaes@gmail.com>
2013-02-17 16:38:37 -0300

    add support to configure jar directory

--- lib/rasper/jar_loader.rb
+++ lib/rasper/jar_loader.rb
@@ -1,10 +1,14 @@
 module Rasper
   class JARLoader
-    def self.load
-      root_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
-      jars_dir = File.join(root_dir, 'java', 'jars')
-      Dir.entries(jars_dir).each do |lib|
-        require File.join(jars_dir, lib) if lib =~ /\.jar$/
+    class << self
+      attr_accessor :jar_dir
+
+      def load
+        root_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
+        jar_dir ||= File.join(root_dir, 'java', 'jars')
+        Dir.entries(jar_dir).each do |lib|
+          require File.join(jar_dir, lib) if lib =~ /\.jar$/
+        end
       end
     end
   end
